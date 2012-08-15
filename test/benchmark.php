8c3b8bb6905c67338a3f44e1b872a03e06782c7e
Nabeel Shahzad <nshahzad@gmail.com>
2012-08-15 12:20:08 -0400

    Added benchmark example

---
+++ test/benchmark.php
@@ -0,0 +1,57 @@
+#!/usr/bin/php
+<?php
+ini_set('date.timezone', 'America/New_York');
+
+include dirname(__FILE__) . '/../src/Codon/Profiler.php';
+
+$data = [];
+for($i = 0; $i < 1000; $i++) {
+	$data[] = $i;
+}
+
+$profiler = new \Codon\Profiler();
+$profiler
+	->set('showOutput', false)
+    ->add([
+        'name' => 'Count in loop',
+        'iterations' => 100,
+        'function' => function() use ($data, &$profiler) {
+
+			$profiler->markMemoryUsage('Start of loop');
+			$profiler->startTimer('Loop only');
+            for($i = 0; $i < count($data); $i++) {
+
+				if($i === 500) {
+					$profiler->checkpoint('halfway');
+					$profiler->markMemoryUsage('halfway');
+				}
+
+                echo $data[$i] . "\n";
+            }
+			$profiler->endTimer('Loop only');
+        }
+    ])
+    ->add([
+        'name' => 'Count out of loop',
+        'iterations' => 100,
+        'function' => function() use ($data, &$profiler) {
+
+			$profiler->markMemoryUsage('Start of loop');
+			$count = count($data);
+
+			$profiler->startTimer('Loop only');
+            for($i = 0; $i < $count; $i++) {
+
+				if($i === 500) {
+					$profiler->checkpoint('halfway');
+					$profiler->markMemoryUsage('halfway');
+				}
+
+                echo $data[$i] . "\n";
+            }
+			$profiler->endTimer('Loop only');
+
+        }
+    ])
+	->run()
+	->showResults();
